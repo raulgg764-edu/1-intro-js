a8c40cf1d7d74f997b3d322c415a8d9887db8341
raulgg764-edu <raul.garzagln@uanl.edu.mx>
2024-09-06 21:20:22 -0600

    Aprender sobre variables y constantes

---
+++ src/main.js
@@ -0,0 +1,15 @@
+//Variables y constantes
+
+let name = "Raul";
+
+name = "Eduardo";
+const lastName= "Garza";
+
+const condition = true;
+
+if(condition){
+    const lastname = "GG";
+    console.log(lastname);
+}
+
+console.log("hola mundo " + name + ' ' + lastName);
